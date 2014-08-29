de277c4911e13a7a99cbc76c9ab423cddfc8424e
perpetuatingcuriosity <perpetuatingcuriosity@gmail.com>
2014-08-29 16:42:55 -0400

    add search fields, find Time Zone function

--- js/app.js
+++ js/app.js
@@ -12,22 +12,27 @@ var query4;
 //Store Lat and Lng results as object in a variable
 var queryResults;
 
+//API Key
+apiKey = "AIzaSyCWNRpeC3rsU6G_xauGKDSo0ZpCEWiGcKw";
+
+
+//----------------------------------------------------------------
+//Init Function
+projectUhr.init = function() {
+	projectUhr.getLocation();
+
+};
+
+
+//----------------------------------------------------------------
+//Find Current Date in Seconds (required for Google Time Zone API)
+
 
 
 
 
 //----------------------------------------------------------------
-
-//API Key
-apiKey = "AIzaSyCWNRpeC3rsU6G_xauGKDSo0ZpCEWiGcKw";
-
-projectUhr.init = function() {
-	projectUhr.getLocation(query1);
-};
-
-
 //Query Google Geocoder API
-
 projectUhr.getLocation = function(searchForThis) {
 	$.ajax({
 		url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' + searchForThis,
@@ -41,16 +46,57 @@ projectUhr.getLocation = function(searchForThis) {
 			// console.log(response.results[0].geometry.location);
 
 			//store only the first result in a variable
-			queryResults = response.results[0].geometry.location;
-			console.log('queryresults are ' + queryResults.lat + ' and ' +  queryResults.lng);
+			queryResults = response.results[0].geometry.location.lat + ',' + response.results[0].geometry.location.lng;
+			queryResults.toString();
+
+			projectUhr.getTimezone(queryResults);
 
 		}
 	});	
 };
 
 
+//----------------------------------------------------------------
+//Query Google Time Zone API
+projectUhr.getTimezone = function(searchForThis) {
+	$.ajax({
+		url: 'https://maps.googleapis.com/maps/api/timezone/json?location='+searchForThis+'&timestamp=1331161200',
+		type: 'GET',
+		data: {
+			key: apiKey,
+		},
+		dataType: 'json',
+		success: function(response){
+			//view query results	
+			console.log(response);
+
+		}
+
+	});
+}
+
+//----------------------------------------------------------------
+//Convert Timezone to Current Time
 
 
+
+
+//----------------------------------------------------------------
+//Display Current Time
+
+
+
+
+
+
+
+//Run projectUhr
 $(function(){
 	projectUhr.init();
 });
+
+
+
+
+
+
